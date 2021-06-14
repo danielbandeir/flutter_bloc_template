118ad09fce0d89bc008c572a0d7dfe520637c301
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2021-06-14 10:32:47 -0300

    Added FToast depency

--- lib/src/shared/data/constants/dependencies.dart
+++ lib/src/shared/data/constants/dependencies.dart
@@ -1,10 +1,16 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:secretwall/src/shared/data/constants/global_keys.dart';
 
 /// Inject global dependencies
-final List<Provider<dynamic>> globalProviders = <Provider<dynamic>>[
-  Provider<GlobalKeys>(create: (BuildContext context) => GlobalKeys()),
-  Provider<Dio>(create: (BuildContext context) => Dio())
+final List<RepositoryProvider<dynamic>> globalProviders = <RepositoryProvider<dynamic>>[
+  RepositoryProvider<GlobalKeys>(create: (BuildContext context) => GlobalKeys()),
+  RepositoryProvider<Dio>(create: (BuildContext context) => Dio(BaseOptions(
+          receiveDataWhenStatusError: true,
+          connectTimeout: 5*1000, // 60 seconds
+          receiveTimeout: 5*1000 
+  ))),
+  RepositoryProvider<FToast>(create: (BuildContext context) => FToast())
 ];
