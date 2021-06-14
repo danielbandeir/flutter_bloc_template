2a42dfda72f8fb35e1df443a18afc0211cf98e91
Daniel Bandeira <bandeirapbdaniel@gmail.com>
2021-06-14 10:31:11 -0300

    Removed MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:secretwall/src/secretwall.dart';
 import 'package:secretwall/src/shared/data/constants/dependencies.dart';
 import 'package:secretwall/src/shared/data/constants/string.dart';
 
 void main() {
   /// Inject global providers before the widget tree
-  runApp(MultiProvider(
+  runApp(MultiRepositoryProvider(
       providers: globalProviders,
       child: const SecretWall(key: Key(secretWallKey))));
 }
